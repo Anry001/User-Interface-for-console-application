58b5293325e4be7bca92fe25887ba62caa238abf
Anry Bentsion <31807291+Anry001@users.noreply.github.com>
2022-02-04 12:44:22 +0200

    Add files via upload

---
+++ User interface for console application/Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Menus.Interfaces
+{
+    public class MainMenu
+    {
+        private string m_ItemTitle = string.Empty;
+        private List<MenuItem> m_Items = new List<MenuItem>();
+
+        public MainMenu(int i_NumberOfItems)
+        {
+            for (int i = 0; i < i_NumberOfItems; i++)
+            {
+                m_Items.Add(new MenuItem());
+            }
+        }
+
+        public MainMenu() {}
+
+        public string ItemTitle
+        {
+            get { return m_ItemTitle; }
+            set { m_ItemTitle = value; }
+        }
+
+        public List<MenuItem> Items
+        {
+            get { return m_Items; }
+        }
+
+        public void Show()
+        {
+            new MenuPresentor(m_Items, ItemTitle);
+        }
+    }
+}
