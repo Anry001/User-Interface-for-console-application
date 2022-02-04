e2ab86dcb98e2778ba3d765e090cb21426f8b19c
Anry Bentsion <31807291+Anry001@users.noreply.github.com>
2022-02-04 11:51:45 +0200

    Add files via upload

---
+++ User interface for console application/Menus.Interfaces/IExecuteOperation.cs
@@ -0,0 +1,7 @@
+﻿namespace Menus.Interfaces
+{
+    public interface IExecuteOperation
+    {
+        void ExecuteOperation();
+    }
+}
---
+++ User interface for console application/Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Menus.Interfaces
+{
+    public class MenuItem : MainMenu
+    {
+        private IExecuteOperation m_OperationObserver = null;
+
+        public MenuItem() {}
+
+        public void AddItems(int i_NumberOfItems) 
+        {
+            for (int i = 0; i < i_NumberOfItems; i++)
+            {
+                this.Items.Add(new MenuItem());
+            }
+            CheckIfItemListEmpty(i_NumberOfItems: i_NumberOfItems);
+        }
+
+        public void AttachOperation(IExecuteOperation i_Operation)
+        {
+            m_OperationObserver = i_Operation;
+            this.AddItems(0);
+        }
+
+        public void CheckIfItemListEmpty(int i_NumberOfItems)
+        {
+            if (i_NumberOfItems == 0)
+            {
+                CheckIfOperationAssigned();
+            }
+        }
+
+        public void ExecuteOperation()
+        {
+            if (m_OperationObserver != null)
+            {
+                m_OperationObserver.ExecuteOperation();
+            }
+            else
+            {
+                Console.WriteLine("assign an operation to {0}.", this.ItemTitle);
+            }
+
+        }
+
+        public void CheckIfOperationAssigned()
+        {
+            if (m_OperationObserver == null)
+            {
+                Console.WriteLine("assign an operation to {0}.", this.ItemTitle);
+            }
+        }
+    }
+}
+
